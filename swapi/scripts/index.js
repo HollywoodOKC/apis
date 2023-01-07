f9320c0fc9e60e22ee13495aa237efda5949b3a5
HollywoodOKC <T_Lane11@yahoo.com>
2023-01-07 15:39:11 -0600

    Working example.

--- swapi/scripts/index.js
+++ swapi/scripts/index.js
@@ -1,13 +1,13 @@
-const axios = require('axios');
+//const axios = require('axios');
 
 const button = document.querySelector('#btn');
 
 const buttonSaying = () => {
-  axios.get('https://swapi.dev/api/').then(res => {
-    for (let t = 0; t < res.length; t++) {
-      axios.get(res).then(response => {
+  axios.get('https://swapi.dev/api/people').then(res => {
+    for (let t = 0; t < res.data.results.length; t++) {
+      axios.get(res.data.results[t].url).then(response => {
         const para = document.createElement("h2");
-        para.innerText = `${response}`;
+        para.innerText = `${JSON.stringify(response.data, null, 2)}`;
         document.body.appendChild(para);
       })
     }
