d087cccf7043dd544c96a54fb7db44dbd2bc9d62
HollywoodOKC <T_Lane11@yahoo.com>
2023-01-07 14:11:50 -0600

    Needs rework.

--- swapi/scripts/index.js
+++ swapi/scripts/index.js
@@ -1,6 +1,17 @@
+const axios = require('axios');
+
 const button = document.querySelector('#btn');
 
 const buttonSaying = () => {
+  axios.get('https://swapi.dev/api/').then(res => {
+    for (let t = 0; t < res.length; t++) {
+      axios.get(res).then(response => {
+        const para = document.createElement("h2");
+        para.innerText = `${response}`;
+        document.body.appendChild(para);
+      })
+    }
+  })
   console.log(`Button Clicked!`);
 }
 
